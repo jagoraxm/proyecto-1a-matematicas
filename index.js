11155377c0935e0880e2cdc7e6a01feb1d3754c2
jonathan.arias <jonathan.arias@lottuseducation.com>
2022-03-05 17:23:30 -0600

    Primera version curso mate

---
+++ index.js
@@ -0,0 +1,46 @@
+module.exports = {
+    /**
+     * Suma de dos numeros
+     * @example
+     * n1 = 1, n2 = 3 => resultado 4
+     * @param {*} n1 Numero uno de la suma
+     * @param {*} n2 Numero dos de la suma
+     * @returns 
+     */
+    suma: function(n1, n2) {
+        return n1 + n2;
+    },
+    /**
+     * Resta de dos numeros
+     * @example
+     * n1 = 1, n2 = 3 => resultado 4
+     * @param {*} n1 Numero uno de la resta
+     * @param {*} n2 Numero dos de la resta
+     * @returns 
+     */
+    resta: function(n1, n2) {
+        return n1 - n2;
+    },
+    /**
+     * Division de dos numeros
+     * @example
+     * n1 = 1, n2 = 3 => resultado 0.3333
+     * @param {*} n1 Numero uno de la division
+     * @param {*} n2 Numero dos de la division
+     * @returns 
+     */
+    division: function(n1, n2) {
+        return n1 / n2;
+    },
+    /**
+     * Multiplicacion de dos numeros
+     * @example
+     * n1 = 1, n2 = 3 => resultado 3
+     * @param {*} n1 Numero uno de la multiplicacion
+     * @param {*} n2 Numero dos de la multiplicacion
+     * @returns 
+     */
+    multiplicacion: function(n1, n2) {
+        return n1 * n2;
+    }
+}
---
+++ pruebas.js
@@ -0,0 +1,3 @@
+const m = require('./index');
+
+console.log(m.multiplicacion(m.division(m.suma(4,6),m.resta(45,34)),100));
